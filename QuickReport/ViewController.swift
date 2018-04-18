5d9d75a27bfb51fb8fd77e070243a1d3bd98a8e1
Atikur Rahman <atik.bd08@gmail.com>
2018-04-18 14:58:31 +0600

    Initial Commit

---
+++ QuickReport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickReport
+//
+//  Created by Atikur Rahman on 18/4/18.
+//  Copyright © 2018 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

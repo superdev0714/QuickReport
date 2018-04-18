1b9f73123d712f3ba2ff977d39754b81b4e13dab
Atikur Rahman <atik.bd08@gmail.com>
2018-04-18 15:17:32 +0600

    get user name when app starts first time

--- QuickReport/ViewController.swift
+++ QuickReport/ViewController.swift
@@ -9,10 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let keyName = "uname"
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let uname = defaults.string(forKey: keyName) {
+            print("hello \(uname)")
+        } else {
+            print("show alert to set user name.")
+            getUserNameAlert()
+        }
+    }
+    
+    func getUserNameAlert() {
+        let alert = UIAlertController(title: "Enter your name", message: nil, preferredStyle: .alert)
+        alert.addTextField {
+            textField in
+            textField.placeholder = "Your Name"
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {[weak alert] (_)in
+            guard let textField = alert?.textFields?.first else {
+                return
+            }
+            
+            guard let name = textField.text, !name.isEmpty else {
+                self.getUserNameAlert()
+                return
+            }
+            
+            self.defaults.set(name, forKey: self.keyName)
+        }))
+        
+        present(alert, animated: true)
     }
 
     override func didReceiveMemoryWarning() {

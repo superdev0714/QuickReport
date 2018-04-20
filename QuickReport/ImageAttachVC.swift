c811ccf02342435c1288833163fb2b004ee19adf
Atikur Rahman <atik.bd08@gmail.com>
2018-04-20 10:00:02 +0600

    handle textview for ImageAttachVC

--- QuickReport/ImageAttachVC.swift
+++ QuickReport/ImageAttachVC.swift
@@ -56,6 +56,26 @@ class ImageAttachVC: UIViewController {
         sendEmail(messageText: messageText, image: previewImageView.image)
     }
     
+    // MARK: -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageDescriptionTextView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        subscribeKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        unsubscribeKeyboardNotifications()
+    }
+    
     // MARK: - Send email
     
     private func sendEmail(messageText: String, image: UIImage?) {
@@ -75,6 +95,42 @@ class ImageAttachVC: UIViewController {
         } else {
             // show failure alert
             print("Email send failed.")
+        }
+    }
+    
+    // MARK: - Subscribe/unsubscribe keyboard notifications
+    
+    private func subscribeKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(keyboardWillShow(notification:)),
+            name: .UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(keyboardWillHide(notification:)),
+            name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    private func unsubscribeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self,
+            name: .UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.removeObserver(self,
+            name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    // MARK: - Handle keyboard notifications
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
         }
     }
 }
@@ -98,5 +154,16 @@ extension ImageAttachVC: UIImagePickerControllerDelegate, UINavigationController
         previewImageView.image = image
         
         dismiss(animated: true)
+    }
+}
+
+extension ImageAttachVC: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
     }
 }

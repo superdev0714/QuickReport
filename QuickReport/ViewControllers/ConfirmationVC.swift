49a1d73036b628a7f850551d24b5cd9e8367f42c
Atikur Rahman <atik.bd08@gmail.com>
2018-04-23 10:24:08 +0600

    reset textview fields after successfully sending mail

--- QuickReport/ViewControllers/ConfirmationVC.swift
+++ QuickReport/ViewControllers/ConfirmationVC.swift
@@ -14,6 +14,17 @@ class ConfirmationVC: UIViewController {
         super.viewDidAppear(animated)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            
+            if let vc = self.navigationController?.viewControllers.first as? TextInputVC {
+                vc.projectNameTextView.text = "Project Name: "
+                vc.applicatorTextView.text = "Applicator: "
+                vc.builderTextView.text = "Builder: "
+                vc.painterTextView.text = "Painter: "
+                vc.substrateTextView.text = "Substrate: "
+                vc.systemTextView.text = "System: "
+                vc.jobSizeTextView.text = "Job size (sqm): "
+            }
+            
             self.navigationController!.popToRootViewController(animated: true)
         }
     }

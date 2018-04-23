2e289b9e3c8f646f1500f52af17bd003e34f6232
Atikur Rahman <atik.bd08@gmail.com>
2018-04-24 00:03:01 +0600

    add actions and outlets for second screen

---
+++ QuickReport/ViewControllers/SecondTextInputVC.swift
@@ -0,0 +1,23 @@
+//
+//  SecondTextInputVC.swift
+//  QuickReport
+//
+//  Created by Atikur Rahman on 23/4/18.
+//  Copyright © 2018 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class SecondTextInputVC: UIViewController {
+    
+    @IBOutlet weak var builderTextView: UITextView!
+    @IBOutlet weak var applicatorTextView: UITextView!
+    @IBOutlet weak var painterTextView: UITextView!
+    @IBOutlet weak var substrateTextView: UITextView!
+    @IBOutlet weak var systemTextView: UITextView!
+    @IBOutlet weak var jobSizeTextView: UITextView!
+    @IBOutlet weak var extraInfoTextView: UITextView!
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+}

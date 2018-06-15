1d3138b272c7ec9d0a787f2adbb83a23926e0805
Atikur Rahman <atik.bd08@gmail.com>
2018-06-15 12:08:56 +0600

    add options to image upload confirmation screen

--- QuickReport/ViewControllers/ImageAddedVC.swift
+++ QuickReport/ViewControllers/ImageAddedVC.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ImageAddedVC: UIViewController {
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.dismiss(animated: true, completion: nil)
+    @IBAction func deletePreviousButtonPressed(_ sender: Any) {
+        if let navController = self.navigationController, navController.viewControllers.count >= 2 {
+            if let vc = navController.viewControllers[navController.viewControllers.count - 2] as? ImageAttachVC {
+                vc.images = []
+            }
         }
+        
+        self.navigationController!.popViewController(animated: true)
+    }
+    
+    @IBAction func uploadAnotherButtonPressed(_ sender: Any) {
+        if let navController = self.navigationController, navController.viewControllers.count >= 2 {
+            if let vc = navController.viewControllers[navController.viewControllers.count - 2] as? ImageAttachVC {
+                self.navigationController!.popViewController(animated: true)
+                vc.cameraButtonPressed(sender)
+            }
+        }
+    }
+    
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        if let navController = self.navigationController, navController.viewControllers.count >= 2 {
+            if let vc = navController.viewControllers[navController.viewControllers.count - 2] as? ImageAttachVC {
+                self.navigationController!.popViewController(animated: true)
+                vc.uploadButtonPressed(sender)
+            }
+        }        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.setHidesBackButton(true, animated:true)
     }
 }

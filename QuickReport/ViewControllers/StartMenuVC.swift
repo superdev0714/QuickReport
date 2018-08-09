3c269fde16642af73ed2fd20f6e4acb4d60f1cab
Atikur Rahman <atik.bd08@gmail.com>
2018-08-09 18:09:28 +0600

    add StartMenuVC

---
+++ QuickReport/ViewControllers/StartMenuVC.swift
@@ -0,0 +1,14 @@
+//
+//  StartMenuVC.swift
+//  QuickReport
+//
+//  Created by Atikur Rahman on 9/8/18.
+//  Copyright © 2018 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class StartMenuVC: UIViewController {
+    
+    
+}

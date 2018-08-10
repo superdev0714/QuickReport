6ba3e5fedd69c2a2ac368792a83e7c9092459fde
Atikur Rahman <atik.bd08@gmail.com>
2018-08-10 09:32:51 +0600

    add SalesRepTextInputVC

---
+++ QuickReport/ViewControllers/SalesRepTextInputVC.swift
@@ -0,0 +1,13 @@
+//
+//  SalesRepTextInputVC.swift
+//  QuickReport
+//
+//  Created by Atikur Rahman on 10/8/18.
+//  Copyright © 2018 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class SalesRepTextInputVC: UIViewController {
+    
+}

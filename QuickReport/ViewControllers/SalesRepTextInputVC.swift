e01077fbb9f142e256d8390e052580243b7fb35a
Atikur Rahman <atik.bd08@gmail.com>
2018-08-10 09:52:50 +0600

    add picker data source

--- QuickReport/ViewControllers/SalesRepTextInputVC.swift
+++ QuickReport/ViewControllers/SalesRepTextInputVC.swift
@@ -10,4 +10,46 @@ import UIKit
 
 class SalesRepTextInputVC: UIViewController {
     
+    var typesOfLearning = [String]()
+    
+    @IBOutlet weak var typeOfLearningPicker: UIPickerView!
+    @IBOutlet weak var learningTextView: UITextView!
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        typesOfLearning = [
+            "Products",
+            "Competitors",
+            "Industry",
+            "Substrates",
+            "Customers/Applicators",
+            "Safety",
+            "Other"
+        ]
+        
+        typeOfLearningPicker.delegate = self
+        typeOfLearningPicker.dataSource = self
+        
+        typeOfLearningPicker.setValue(UIColor.black, forKey: "textColor")
+        typeOfLearningPicker.backgroundColor = .white
+    }
+}
+
+extension SalesRepTextInputVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return typesOfLearning.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return typesOfLearning[row]
+    }
 }
